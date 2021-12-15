df38acbf217bb152198364810e4340451223d6a8
lama1997-coder <lamayousef1997@gmail.com>
2021-12-15 14:47:21 +0200

    first commit

---
+++ lib/ray_world_game.dart
@@ -0,0 +1,47 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+import 'components/player.dart';
+import 'components/world.dart';
+import 'components/world_collidable.dart';
+import 'helpers/direction.dart';
+import 'helpers/map_loader.dart';
+import 'components/world_collidable.dart';
+import 'helpers/map_loader.dart';
+import 'package:flame/components.dart';
+
+
+
+class RayWorldGame extends FlameGame with HasCollidables {
+  final Player _player = Player();
+  final World _world = World();
+
+
+ @override
+ Future<void> onLoad() async {
+   // empty
+   await add(_world);
+
+   add(_player);
+   addWorldCollision();
+
+
+   _player.position = _world.size / 2;
+   camera.followComponent(_player,
+       worldBounds: Rect.fromLTRB(0, 0, _world.size.x, _world.size.y));
+
+ }
+
+ void onJoypadDirectionChanged(Direction direction) {
+   _player.direction = direction;
+}
+
+void addWorldCollision() async =>
+     (await MapLoader.readRayWorldCollisionMap()).forEach((rect) {
+       add(WorldCollidable()
+         ..position = Vector2(rect.left, rect.top)
+         ..width = rect.width
+         ..height = rect.height);
+     });
+
+}
